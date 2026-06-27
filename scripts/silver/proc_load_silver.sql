56b30239ff07dd7f2b8c6dd434b9623faf6edcb9
sourab27 <SourabSadashiv.Marabasannavar@rockwellautomation.com>
2026-06-27 10:14:49 +0530

    Load cleaned data into silver.crm_cust_info
    Insert cleaned customer data from bronze to silver layer with transformations.

--- scripts/silver/proc_load_silver.sql
+++ scripts/silver/proc_load_silver.sql
@@ -1,1 +1,44 @@
 
+--query for loading the cleaned data from bronze layer to silver layer
+-- New query with above sub queries
+
+
+
+INSERT INTO silver.crm_cust_info(
+    cst_id,
+    cst_key,
+    cst_firstname,
+    cst_lastname,
+    cst_marital_status,
+    cst_gndr,
+    cst_create_date)
+
+SELECT
+    cst_id,
+    cst_key,
+    TRIM(cst_firstname) AS cst_firstname,
+    TRIM(cst_lastname) AS cst_lastname,
+    
+    CASE 
+        WHEN UPPER(TRIM(cst_marital_status)) = 'S' THEN 'Single'
+        WHEN UPPER(TRIM(cst_marital_status)) = 'M' THEN 'Married'
+        ELSE 'N/A'
+    END AS cst_marital_status,
+    
+    CASE 
+        WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
+        WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
+        ELSE 'N/A'
+    END AS cst_gndr,
+    
+    cst_create_date
+FROM (
+    SELECT *,
+           ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) AS flag_last
+    FROM bronze.crm_cust_info
+) t
+WHERE flag_last = 1;
+
+
+
+
